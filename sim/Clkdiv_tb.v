c693cdf2ba115956d984219053b870bc0b664236
Xinhua <1073127884@qq.com>
2022-12-07 14:11:15 +0800

    renamed:    src/Clkdiv_tb.v -> sim/Clkdiv_tb.v 	modified:   src/Clkdiv.v

--- sim/Clkdiv_tb.v
+++ sim/Clkdiv_tb.v
@@ -2,10 +2,11 @@
 module Clkdiv_tb ();
     reg clk;
     wire clk_alu;
+    wire clk_1M;
 
     always #5 clk = ~clk;
 
-    Clkdiv clkdiv(.clk_100M(clk),.clk_alu(clk_alu));
+    Clkdiv clkdiv(.clk_100M(clk),.clk_alu(clk_alu),.clk_1M(clk_1M));
 
     initial begin
         clk = 0;
--- src/Clkdiv.v
+++ src/Clkdiv.v
@@ -3,33 +3,49 @@ module Clkdiv
     #(
         parameter     N = 9999_9999,
         parameter     div1 = 100,
-        parameter     div2 = 95
+        parameter     div2 = 95,
+        parameter     div3 = 100,
+        parameter     div4 = 50
     )
     (
         input         clk_100M,
-        output reg    clk_alu //1MHz
+        output reg    clk_alu, //1MHz
+        output reg    clk_1M
     );  
-        reg [31:0]    count;
+        reg [31:0]    count1 ,count2;
         
         initial begin
-            count = 0;
-            clk_alu = 0;
+            count1 = 0;
+            clk_alu = 1;
+            clk_1M = 1;
         end
 
         always @(posedge clk_100M) begin
-            if (count < div2) begin
-                count <= count + 1;
+            if (count1 < div2) begin
+                count1 <= count1 + 1;
             end
-            else if (count >= div2 && count <= div1 ) begin
+            else if (count1 >= div2 && count1 <= div1 ) begin
                 clk_alu <= 0;
-                count <= count + 1; 
+                count1 <= count1 + 1; 
             end 
             else begin
                 clk_alu <= 1;
-                count <= 0;
+                count1 <= 0;
             end
+        end
 
-            
+        always @(posedge clk_100M) begin
+            if (count2 < div4) begin
+                count2 <= count2 + 1;
+            end
+            else if (count2 >= div4 && count2 <= div3 ) begin
+                clk_1M <= 0;
+                count2 <= count2 + 1; 
+            end 
+            else begin
+                clk_1M <= 1;
+                count2 <= 0;
+            end
         end
         
 
