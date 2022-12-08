45208297ad0a87f496d6ea9ad5b8fb5da980fa00
7zzd@linux <a126686439@gmail.com>
2022-12-09 00:57:11 +0800

    add_c_source_file

---
+++ compile/converter.py
@@ -0,0 +1,25 @@
+import re
+import struct
+target=input("please input the binary file's path:")
+while(1):
+    try:
+        binaryfile=open(target,"rb")
+    except:
+        print('An wrong occurred,try again')
+        target=input("please input the binary file's path:")
+    else:
+        print('successfully read this file!')
+        break
+outputname=(re.search(r'([^/]*)\.bin$',target)).group(1)
+outputname=outputname+'.mem'
+outputfile=open(outputname,'w')
+bin_str=binaryfile.read(4)
+while(1):
+    try:
+      tmp_tup=struct.unpack('1I',bin_str)
+    except:
+      break
+    outputfile.write('%08x\n'%(tmp_tup[0]))
+    bin_str=binaryfile.read(4)
+binaryfile.close()
+outputfile.close()
---
+++ compile/test.c
@@ -0,0 +1,11 @@
+#include <stdint.h>
+int main(){
+    int a=40;
+    int b=30;
+    int c=a+b;
+    int d=a-b;
+    int e=a*b;
+    int f=a/b;
+    int h=a%b;
+    return 0;
+}
