16018dbbf6852863a53091e51cd4b0a406cd6d16
Yuchen-10001 <1597821743@qq.com>
2023-01-06 16:02:10 +0800

    final ps2_mouse

--- src/ps2_read_data.v
+++ src/ps2_read_data.v
@@ -1,122 +1,81 @@
-module Ps2_read_data 
-    (
-        input                done,
-        input                clk,
-        input                rstn,
-        input                ps2_clk,
-        input                ps2_data,
-        output reg [15:0]    x_position,
-        output reg [15:0]    y_position,
-        output reg [7:0]     LBM
-    );
-        reg          ps2_clkf;
-        reg          ps2_clkb;
-        reg [15:0]   x_addr;
-        reg [15:0]   y_addr;
-        reg [23:0]   data;
-        reg [3:0]    count0;
-        reg [1:0]    count1;
-        reg [2:0]    state;
-        reg          clk_ps2;
-        //检测ps2_clk的下降沿
-        always @(posedge clk) begin
-            ps2_clkf <= ps2_clk;
-            ps2_clkb <= ps2_clkf;
-        end 
-        wire ps2_clk_negfetch = ! ps2_clkf && ps2_clkb;
+`timescale 1ns / 1ps
 
-        //状态机
-        always @(posedge clk or negedge rstn) begin
-            if (!rstn) begin
-                x_addr <= 'd0;
-                y_addr <= 'd0;
-                state  <= 'd0;
-                data   <= 'd0;
-                count0 <= 'd0;
-            end
-            else begin
-                case (state)
-                    'd0: begin
-                        if (done) begin
-                            state <=3'd1;
-                        end
-                        else begin
-                            count1 <= 2'd0;
-                            state <= state;
-                        end
-                    end 
-                    //start信号：在ps2_data=0时产生一个ps2_clk的下降沿
-                    'd1: begin
-                        if (!ps2_data && ps2_clk_negfetch) begin
-                            state <= 'd2;
-                        end
-                        else begin
-                            state <= state;
-                        end
-                    end
-                    //读24位数据
-                    'd2: begin
-                        if (ps2_clk_negfetch) begin
-                            data <= {ps2_data,data[23:1]};
-                            if (count0 == 'd7) begin
-                                state <= 'd3;
-                                count0 <= 'd0;
-                            end
-                            else begin
-                                count0 <= count0 + 1'b1;
-                            end
-                        end
-                    end
-                    //奇偶校验位，忽略
-                    'd3: begin
-                        if(ps2_clk_negfetch) begin
-                            state <= state + 1'b1;
-                        end
-                        else begin
-                            state <= state;
-                        end
-                    end
-                    //字节停止信号，忽略，但需要判断ps2_data的值
-                    'd4: begin
-                        if(ps2_clk_negfetch && ps2_data) begin
-                            state <= state + 1'b1; 
-                        end
-                        else begin
-                            state <= state + 1'b1;
-                        end
-                    end
-                    //需要读3字节的数据，因此要使得count1变成3
-                    'd5: begin
-                        if (count1 == 'd2) begin
-                            count1 <= 'd0;
-                            state <= 'd0;
-                            clk_ps2 <= ~clk_ps2;
-                            x_addr <= { {8{data[4]}} , data[15:8] };
-                            y_addr <= { {8{data[5]}} , data[23:16] };
-                            LBM <= {{7'b0},{data[0]}};
-                        end
-                        else begin
-                        if(count1 == 'd1) begin
-                            clk_ps2 <= ~clk_ps2;
-                        end
-                            state <= 3'd0;
-                            count1 <= count1 + 'd1;
-                        end 
-                    end
-                    default: state <= state;
-                endcase
-            end
-        end
+module ps2_read_funcmod(
+    input CLOCK, RESET,
+    input PS2_CLK,PS2_DAT,
+    input iEn,
+    output oTrig,
+    output [23:0]oData
+    );  
 
-        //x，y在vga屏幕上的坐标位置。
-        always @(posedge clk_ps2 or negedge rstn) begin
-            if (!rstn) begin
-                x_position <= 'd0;
-                y_position <= 'd0;
-            end
-            else  begin
-                x_position <= (x_position + x_addr);
-                y_position <= (y_position + y_addr);
-            end
+    parameter FF_Read = 7'd32; //伪函数的入口地址
+    /*******************************/ // sub1
+ 
+    reg F2,F1; 
+    always @ ( posedge CLOCK or negedge RESET )
+        if( !RESET )
+            { F2,F1 } <= 2'b11;
+        else 
+            { F2, F1 } <= { F1, PS2_CLK };
+
+    /*******************************/ // core
+             
+    wire isH2L = ( F2 == 1'b1 && F1 == 1'b0 );//检测电平变化的周边操作。下降沿的即时声明。
+
+    reg [23:0]D1;
+    reg [7:0]T;
+    reg [6:0]i,Go;
+    reg isDone;
+    always @ ( posedge CLOCK or negedge RESET )
+        if( !RESET ) begin //相关的寄存器声明以及复位操作
+            D1 <= 24'd0;
+            T <= 8'd0;
+            { i,Go } <= { 7'd0,7'd0 };
+            isDone <= 1'b0;
         end
-endmodule 
+        else if( iEn )  
+        case( i )             
+        /*********/ // Normal mouse 第36行 if( iEn ) 表示，iEn不拉高核心操作就不运行。步骤0~1是读取第一字节，步骤2~3是读取第二字节，步骤4~5是读取第三字节，步骤6~7则是反馈完成信号，以示一次性的报告读取已经完成。完后，i便指向步骤0。
+            0: // Read 1st byte
+                begin i <= FF_Read; Go <= i + 1'b1; end
+
+            1: // Store 1st byte
+                begin D1[7:0] <= T; i <= i + 1'b1; end
+
+            2: // Read 2nd byte
+                begin i <= FF_Read; Go <= i + 1'b1; end
+
+            3: // Store 2nd byte
+                begin D1[15:8] <= T; i <= i + 1'b1; end
+  
+            4: // Read 3rd byte
+                begin i <= FF_Read; Go <= i + 1'b1; end
+
+            5: // Store 3rd byte
+                begin D1[23:16] <= T; i <= i + 1'b1; end
+
+            6:
+                begin isDone <= 1'b1; i <= i + 1'b1; end
+   
+            7:
+                begin isDone <= 1'b0; i <= 7'd0; end                       
+        /****************/ // PS2 Write Function 步骤32~42则是伪函数，主要是负责读取一帧数据
+            
+            32: // Start bit
+                if( isH2L ) i <= i + 1'b1; 
+
+            33,34,35,36,37,38,39,40:  // Data byte
+                if( isH2L ) begin T[i-33] <= PS2_DAT; i <= i + 1'b1;  end
+
+            41: // Parity bit
+                if( isH2L ) i <= i + 1'b1;
+
+            42: // Stop bit
+                if( isH2L ) i <= Go;
+           
+        endcase
+                 
+    assign oTrig = isDone;
+    assign oData = D1;
+   
+endmodule
