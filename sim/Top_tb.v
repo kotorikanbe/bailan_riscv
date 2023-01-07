4788fe269a2fa835d4c971d48aa010b29e80721f
Xinhua <1073127884@qq.com>
2023-01-07 23:52:32 +0800

    modified:   sim/Top_tb.v

--- sim/Top_tb.v
+++ sim/Top_tb.v
@@ -9,22 +9,14 @@ module Top_tb ();
         wire  [3:0]           vga_o_green;
         wire                  h_sync;
         wire                  v_sync;
-        wire  [31:0]          inst;
 
-    Riscv_top riscv_top(clk,rst_n,ps2_clk,ps2_data,vga_o_red,vga_o_blue,vga_o_green,h_sync,v_sync,inst);
+    Riscv_top riscv_top(clk,rst_n,ps2_clk,ps2_data,vga_o_red,vga_o_blue,vga_o_green,h_sync,v_sync);
 
     always #5 clk=~clk;
     initial begin
         rst_n = 0; clk = 0;
         #3 rst_n =1;
     end
- integer fd;
- initial begin
- fd = $fopen("C:/Users/Winter/Desktop/picture" , "w");
- end
- always @(*) begin
- $fdisplay(fd,"%h" , inst);
- end
 
 
 
